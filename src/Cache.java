74c1620f15c055ee7293674f8ad10b24fe5b54c2
Yahia El Gamal <yahiaelgamal@gmail.com>
2013-01-09 23:43:04 +0200

    added and tested mapping address to cache (tag, index, offset)

--- src/Cache.java
+++ src/Cache.java
@@ -1,30 +1,69 @@
 public class Cache {
 	
-	int numLines;
+	int size;
+	int lineSize;
+	int associativity;
+	int hitPolicy; // 0 for write back, 1 for write through
+	int missPolicy; // 0 for write allocate, 1 for write around
 	int hitTime;
 	int penalty;
-	int associativity;
-	int policy; // 0 for write back, 1 for write through
+	
+	int numLines;
 	int[] map;
 	int[] dirtyBits;
 	
-	public Cache(int numLines, int hitTime, int penalty, int assoc, int policy) {
-		this.numLines 	= numLines;
+	public Cache(int size, int lineSize, int hitTime, int penalty, int assoc, int hitPolicy) {
+		this.size 		= size;
 		this.hitTime 	= hitTime;
-		this.penalty 	= penalty;
 		this.associativity = assoc;
-		this.policy 	= policy;
+		this.penalty 	= penalty;
+		this.hitPolicy 	= hitPolicy;
 		
+		numLines = size/lineSize;
 		map = new int[numLines];
 		dirtyBits = new int[numLines];
 	}
 	
-	public Integer read(Integer address) {
-		if (map[address] != 0) {
-			return map[address];
-		}else {
-			// false
-			return null;
+	/*
+	 * this will return a number that has the binary value of n
+	 * consecutive ones. Example, n = 3 will return 0b111 = 7 
+	 * 
+	 */
+	public static int makeNOnes(int n) {
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			res += Math.pow(2, i);
 		}
+		return res;
 	}
+	
+	public static int log2(int n) {
+		return (int) (Math.log10(n)/Math.log10(2));
+	}
+	
+	// returns array = {tag, index, offset}
+	public int[] map(int address) {
+		// TESTED AND  WORKING (all sheet examples were tests)
+		
+		int offset = address & makeNOnes(log2(lineSize));
+		offset <<= 2;
+		int index 	= address & (makeNOnes(log2(numLines/associativity)) << log2(lineSize));
+		index = index >> log2(lineSize);
+		
+		int tag = address >> ((log2(lineSize) + log2(numLines/associativity)));
+		
+//		System.out.println(Integer.toBinaryString(tag) + " " + Integer.toBinaryString(index) + " " + Integer.toBinaryString(offset));
+//		System.out.println(tag + " " + index + " " + offset);
+		return new int[] {tag, index, offset};
+	}
+	
+	public Integer read(Integer address) {
+		int[] tagIndexOffset = map(address);
+		// handle hit, miss
+		return null;
+	}
+	
+	public void write(Integer address, Integer value) {
+	}
+	
 }
--- src/DataFetcher.java
+++ src/DataFetcher.java
@@ -1,4 +1,3 @@
-
 public class DataFetcher {
 	/*
 	 * This is a container for the caches and memory
@@ -9,6 +8,7 @@ public class DataFetcher {
 	Memory mem;
 	
 	public DataFetcher(int levels, int numlines1, int numlines2, int numlines3,
+  					   int lineSize1,  int lineSize2, int lineSize3,
   					   int hitTime1,  int hitTime2, int hitTime3,
 					   int penalty1, int penalty2, int penalty3, 
 					   int assoc1, int assoc2, int assoc3,
@@ -17,12 +17,13 @@ public class DataFetcher {
 		caches = new Cache[levels];
 		
 		if (levels <= 1)
-			caches[0] = new Cache(numlines1, hitTime1, penalty1, assoc1, policy1);
+			caches[0] = new Cache(numlines1, lineSize1, hitTime1, penalty1, assoc1, policy1);
 		if (levels <= 2)
-			caches[1] = new Cache(numlines2, hitTime2, penalty2, assoc2, policy2);
+			caches[1] = new Cache(numlines2, lineSize2, hitTime2, penalty2, assoc2, policy2);
 		if (levels <= 3)
-			caches[2] = new Cache(numlines3, hitTime3, penalty3, assoc3, policy3);
+			caches[2] = new Cache(numlines3, lineSize3, hitTime3, penalty3, assoc3, policy3);
 	}
+	
 	
 	public int fetch(int address) {
 		for(int i = 0; i < levels; i++) {
