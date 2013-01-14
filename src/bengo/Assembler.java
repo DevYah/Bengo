8617f772970c3f0e97bfc25e70dbb6aa23c9985f
Mostafa Mahmoud <mostafa.mahmoud@g-beehive.com>
2013-01-14 04:23:51 +0200

    Fixing checking address, registers in Assembler.

--- src/bengo/Assembler.java
+++ src/bengo/Assembler.java
@@ -4,14 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Assembler {
 	static final String[] commands = {"LW", "SW", "JMP", "BEQ", "JALR", "RET", "ADD", "ADDI", "NAND", "MUL", "DIV"};
 	static final int[] commandArgsCount = {3, 3, 2, 3, 2, 1, 3, 3, 3, 3, 3};
-	static final int[] commandRegCount = {};
+	static final int[] commandRegCount = {2, 2, 1, 2, 2, 1, 3, 2, 3, 3, 3};
+	static final String[] REGS = {"R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7"};
 	
-	String FILE_NAME, error, args[];
+	String FILE_NAME, error;
+	Instruction[] program;
 	
 	public Assembler(String file) {
 		FILE_NAME = file;
@@ -19,17 +22,22 @@ public class Assembler {
 	}
 	
 	public boolean assemble() throws IOException {
+		//TODO Test
 		BufferedReader std = new BufferedReader(new FileReader(new File(FILE_NAME)));
-		Pattern p = Pattern.compile("[A-Z]+ (?:[a-zA-Z_0-9]+, )*(?:[a-zA-Z_0-9]+)");
+		Pattern commandMatcher = Pattern.compile("[A-Z]+ (?:[a-zA-Z_0-9]+, )*(?:[a-zA-Z_0-9]+)");
+		// FIXME: Handle negative numbers.
+		Pattern intMatcher = Pattern.compile("-?[0-9]+");
+		ArrayList<Instruction> instructions = new ArrayList<Instruction>();
+		program = null;
 		int lineNum = 1;
 		while (true) {
 			String inst = std.readLine();
 			if (inst == null)
 				break;
-			if (!p.matcher(inst).matches())
+			if (!commandMatcher.matcher(inst).matches())
 				return setErrorMessage(lineNum, "Invalid syntax");
 			
-			args = inst.split(" ");
+			String[] args = inst.split(" ");
 			for (int i = 0; i < args.length; ++i)
 				args[i] = args[i].split(",")[0];
 			
@@ -44,17 +52,24 @@ public class Assembler {
 			if (args.length - 1 != commandArgsCount[command_ind])
 				return setErrorMessage(lineNum, String.format("Wrong number of arguments, %d instead of %d.", args.length - 1, commandArgsCount[command_ind]));
 			
-			for (int i = 0; i < commandRegCount[command_ind]; ++i)
-				if (args[i + 1] == "0") // FIXME: Check valid registers
-					return setErrorMessage(lineNum, String.format("Invalid register No. %d", i + 1));
-		    for (int i = 0; i < commandArgsCount[command_ind] - commandRegCount[command_ind]; ++i) {
-                if (args[args.length - 1 - i] == "0") // FIXME: Check valid address
-                    return setErrorMessage(lineNum, String.format("Invalid address %d", args.length - 1 - i));
-            }
-			//TODO Create instruction with the arguments
-		
+			for (int i = 0; i < commandRegCount[command_ind]; ++i) {
+				boolean valid = false;
+				for (int j = 0; j < REGS.length; ++j)
+					if (args[i + 1].equals(REGS[j])) {
+						valid = true;
+						break;
+					}
+				if (!valid)
+					return setErrorMessage(lineNum, String.format("Invalid register name, argument #%d", i + 1));
+			}
+			for (int i = 0; i < commandArgsCount[command_ind] - commandRegCount[command_ind]; ++i) {
+				if (!intMatcher.matcher(args[args.length - 1 - i]).matches())
+					return setErrorMessage(lineNum, String.format("Invalid argument, argument #%d", args.length - 1 - i));
+			}
+			instructions.add(new Instruction(args, lineNum - 1));
 			++lineNum;
 		}
+		program = (Instruction[]) instructions.toArray();
 		return true;
 	}
 	
@@ -63,10 +78,9 @@ public class Assembler {
 	}
 	public boolean setErrorMessage(int lineNum, String e) {
 		error = String.format("Error in Line No. %d : %s", lineNum, e);
-		args = null;
 		return false;
 	}
-	public String[] getArguments() {
-		return args;
+	public Instruction[] gerProgram() {
+		return program;
 	}
 }
